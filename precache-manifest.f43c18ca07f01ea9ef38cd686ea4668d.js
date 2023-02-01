3caabb40d49ea443767095864b352352715db0c6
Aryamann Singh <aryamann.singh21@gmail.com>
2023-02-02 00:32:44 +0530

    Updates

---
+++ precache-manifest.f43c18ca07f01ea9ef38cd686ea4668d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ba2f334025b7d1409852",
+    "url": "/TextUtils-React/static/css/main.f273b0b5.chunk.css"
+  },
+  {
+    "revision": "ba2f334025b7d1409852",
+    "url": "/TextUtils-React/static/js/main.ba2f3340.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/TextUtils-React/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/TextUtils-React/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "2591e466b5f270c0c477",
+    "url": "/TextUtils-React/static/js/runtime~main.2591e466.js"
+  },
+  {
+    "revision": "b562206437151841fd0328530ba49528",
+    "url": "/TextUtils-React/index.html"
+  }
+];
